bda9085eaba512b5f5bbc89a6cb67de3ba80321b
JeromeCalicdan <jeromecalics@gmail.com>
2023-06-26 10:52:26 +0800

    lab express

---
+++ models/models/index.js
@@ -0,0 +1,44 @@
+let users = {
+    1: {
+        id: '1',
+        username: 'Robin Hood'
+    },
+    2: {
+        id: '2',
+        username: 'Arue'
+    }
+}
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1'
+    },
+    2: {
+        id: '2',
+        text: 'By World',
+        userId: '2'
+    }
+}
+
+let quotes = {
+    1: {
+        id: 1,
+        quote: 'EMPTINESS COMES AS A SUNSET COMES',
+        author: 'Krishnamurti in Madras',
+        year: ' 1964'
+    },
+    2: {
+        id: 2,
+        quote: 'WE Are Family',
+        author: 'Vin Diesel',
+        year: ' 2022'
+    }
+}
+
+export default {
+    users,
+    messages,
+    quotes,
+}
